7f20a811b3b99be5648fd04d827145ef019c1c16
sathiya <sathiyaseelan7082@gmail.com>
2026-04-07 17:17:48 +0530

    Revert GitHub pages config for Vercel deployment

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,10 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
-  basePath: "/Hybrid-Quantum-Classical-Diabetic-Retinopathy-Detection-System",
-  images: {
-    unoptimized: true,
-  },
   eslint: { ignoreDuringBuilds: true },
   typescript: { ignoreBuildErrors: true },
 };
